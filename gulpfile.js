b007e6ad2ccb8802faddf37debf5fd35dca5e2f2
littxx <ederlanbrito@gmail.com>
2023-11-24 10:11:57 -0300

    opa

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,24 @@
 const gulp = require("gulp");
-const clean = require("gulp-clean");
+const rimraf = require("gulp-rimraf");
 const ts = require("gulp-typescript");
 
-const tsProjetc = ts.createProject("tsconfig.json");
+const tsProject = ts.createProject("tsconfig.json");
 
 gulp.task("script", () => {
-  const tsResult = tsProjetc.src().pipe(tsProjetc());
+  const tsResult = tsProject.src().pipe(tsProject());
 
   return tsResult.js.pipe(gulp.dest("dist"));
 });
+
+gulp.task("static", () => {
+  return gulp.src(["src/**/*.json"]).pipe(gulp.dest("dist"));
+});
+
+gulp.task("clean", () => {
+  return gulp.src("dist", { read: false, allowEmpty: true }).pipe(rimraf());
+});
+
+gulp.task("build", gulp.series("clean", "static", "script"));
+
+// Tarefa padrão (default)
+gulp.task("default", gulp.series("build"));
--- src/App.ts
+++ src/App.ts
@@ -1,1 +1,1 @@
-console.log("hello world");
+console.log("um deus cosmisco");
